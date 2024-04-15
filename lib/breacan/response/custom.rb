a8399239f08bb61d1b832e7b8fcf8f76b009b9d4
kazu <kazuhito-nakayama@pepabo.com>
2024-04-15 19:27:58 +0900

    fix: use appropriate class

--- lib/breacan/response/custom.rb
+++ lib/breacan/response/custom.rb
@@ -7,7 +7,7 @@ module Breacan
     # ref: https://github.com/lostisland/faraday/blob/c9cc1b30ecebcd57beffa67d275f68742b5b05c5/UPGRADING.md?plain=1#L128
     middleware_base = defined?(Faraday::Response::Middleware) ? Faraday::Response::Middleware : Faraday::Middleware
 
-    class Custom < Faraday::Response::Middleware
+    class Custom < middleware_base
       private
 
       def on_complete(res)
